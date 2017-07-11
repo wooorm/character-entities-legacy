68fc5d0b299d1c4d0326a74e7ca1dfd50a6ee2a7
Titus Wormer <tituswormer@gmail.com>
2017-07-11 19:26:24 +0200

    Refactor `build.js`

--- build.js
+++ build.js
@@ -8,20 +8,20 @@ var concat = require('concat-stream');
 var url = 'https://raw.githubusercontent.com/whatwg/html/cb8445520f10e044d23cfd3ff7fc38e064edf1f1/json-entities-legacy.inc';
 
 https.get(url, function (res) {
-  res
-    .pipe(concat(function (data) {
-      var entities = {};
+  res.pipe(concat(onconcat)).on('error', bail);
 
-      data = data.slice(0, -2); // Remove trailing comma.
-      data = JSON.parse('{' + data + '}');
+  function onconcat(data) {
+    var entities = {};
 
-      Object.keys(data).forEach(function (key) {
-        entities[key.slice(1)] = data[key].characters;
-      });
+    data = data.slice(0, -2); // Remove trailing comma.
+    data = JSON.parse('{' + data + '}');
 
-      data = JSON.stringify(entities, 0, 2);
+    Object.keys(data).forEach(function (key) {
+      entities[key.slice(1)] = data[key].characters;
+    });
 
-      fs.writeFile('index.json', data + '\n', bail);
-    }))
-    .on('error', bail);
+    data = JSON.stringify(entities, 0, 2);
+
+    fs.writeFile('index.json', data + '\n', bail);
+  }
 });
