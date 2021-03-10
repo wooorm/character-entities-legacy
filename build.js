c53396c91c44ee4df3dd04c739825cbde93f7503
Titus Wormer <tituswormer@gmail.com>
2021-03-10 11:36:00 +0100

    Update dev-dependencies

--- build.js
+++ build.js
@@ -5,6 +5,8 @@ var https = require('https')
 var bail = require('bail')
 var concat = require('concat-stream')
 
+var own = {}.hasOwnProperty
+
 https.get(
   'https://raw.githubusercontent.com/whatwg/html-build/HEAD/entities/json-entities-legacy.inc',
   onconnection
@@ -16,13 +18,18 @@ function onconnection(response) {
 
 function onconcat(data) {
   var entities = {}
+  var values = JSON.parse(
+    '{' +
+      data.slice(0, -2) + // Remove trailing comma.
+      '}'
+  )
+  var key
 
-  data = data.slice(0, -2) // Remove trailing comma.
-  data = JSON.parse('{' + data + '}')
-
-  Object.keys(data).forEach(function (key) {
-    entities[key.slice(1)] = data[key].characters
-  })
+  for (key in values) {
+    if (own.call(values, key)) {
+      entities[key.slice(1)] = values[key].characters
+    }
+  }
 
   data = JSON.stringify(entities, null, 2)
 
